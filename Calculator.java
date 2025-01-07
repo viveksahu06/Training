20b6ec2d5cebece837aa894635b6ce7cd2815277
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 15:22:30 +0530

    adding feature

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,6 @@
 import java.util.*; 
 public class Calculator{
-	public static void main(String []args){
-		int a=10; 
-		int b=20;  
-		char op='+'
-		if(op=='+') System.out.println(sum(a,b)); 
-		else if(op=='-') System.out.println(sub(a,b));
-		else if(op=='*') System.out.println(mul(a,b));
-		else if(op=='/') System.out.println(div(a,b));  
-		else System.out.println("enter correct ope");
+	public static int sum(int a,int b){
+		return a+b;
 	}
 }
