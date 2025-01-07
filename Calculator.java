670fb324a88075c369a0763b1ea24e64a5ba2395
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 15:31:42 +0530

    untrack

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,9 @@
-import java.util.*; 
-public class Calculator{
-	public static int sub(int a,int b){
-		return a-b;
-	}
+public class Calculator {
+   
+    public static void main(String[] args) {
+        System.out.println("Addition: " + add(10, 5));
+        System.out.println("Subtraction: " + subtract(10, 5));
+        System.out.println("Multiplication: " + multiply(10, 5));
+        System.out.println("Division: " + divide(10, 5));
+    }
 }
