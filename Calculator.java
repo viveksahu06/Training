e2d8a0093347d9592612036bc95b9846e9e16ebc
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 15:37:57 +0530

    Add skeleton code for Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,5 @@
 public class Calculator {
-   
-    public static int multiply(int a, int b) {
-        return a * b;
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Calculator!");
     }
 }
