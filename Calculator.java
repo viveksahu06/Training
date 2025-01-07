724013ab128a6d95dca3dd838f32e19e0e885258
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 15:34:30 +0530

    Add multiplication functionality

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,6 @@
 public class Calculator {
    
-    public static void main(String[] args) {
-        System.out.println("Addition: " + add(10, 5));
-        System.out.println("Subtraction: " + subtract(10, 5));
-        System.out.println("Multiplication: " + multiply(10, 5));
-        System.out.println("Division: " + divide(10, 5));
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 }
